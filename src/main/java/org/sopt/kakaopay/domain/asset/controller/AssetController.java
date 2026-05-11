fbbd92378284fb213e9a8be64957d0e93c4f34c5
Jyunee54 <jyunee54@gmail.com>
2026-05-12 03:38:13 +0900

    init(#1): 어노테이션 추가

--- src/main/java/org/sopt/kakaopay/domain/asset/controller/AssetController.java
+++ src/main/java/org/sopt/kakaopay/domain/asset/controller/AssetController.java
@@ -1,4 +1,14 @@
 package org.sopt.kakaopay.domain.asset.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.sopt.kakaopay.domain.asset.service.AssetService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/asset")
+@RequiredArgsConstructor
 public class AssetController {
+
+    private final AssetService assetService;
 }
--- src/main/java/org/sopt/kakaopay/domain/expense/service/ExpenseService.java
+++ src/main/java/org/sopt/kakaopay/domain/expense/service/ExpenseService.java
@@ -1,4 +1,11 @@
 package org.sopt.kakaopay.domain.expense.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ExpenseService {
 }
