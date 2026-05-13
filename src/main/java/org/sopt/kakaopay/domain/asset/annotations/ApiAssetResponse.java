961c149159dcdb60a2bca0cdff28f256bb940400
Jyunee54 <jyunee54@gmail.com>
2026-05-13 19:00:27 +0900

    feat(#1): Swagger 응답 예시 추가

--- src/main/java/org/sopt/kakaopay/domain/asset/annotations/ApiAssetResponse.java
+++ src/main/java/org/sopt/kakaopay/domain/asset/annotations/ApiAssetResponse.java
@@ -2,11 +2,11 @@ package org.sopt.kakaopay.domain.asset.annotations;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.sopt.kakaopay.domain.asset.dto.response.AssetResponse;
-
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -20,7 +20,36 @@ import java.lang.annotation.Target;
         @ApiResponse(
                 responseCode = "200",
                 description = "자산 조회 성공",
-                content = @Content(schema = @Schema(implementation = AssetResponse.class))
+                content = @Content(
+                        schema = @Schema(implementation = AssetResponse.class),
+                        examples = @ExampleObject(
+                                name = "자산 조회 성공 예시",
+                                value = """
+                                        {
+                                          "code": "AST_200",
+                                          "message": "자산 조회에 성공했습니다.",
+                                          "data": {
+                                            "kakaopayBalance": 12300,
+                                            "favoriteAccounts": [
+                                              {
+                                                "accountId": 1,
+                                                "accountInfo": "카카오뱅크 1785"
+                                              },
+                                              {
+                                                "accountId": 2,
+                                                "accountInfo": "토스뱅크 0000"
+                                              },
+                                              {
+                                                "accountId": 3,
+                                                "accountInfo": "대구은행 0000"
+                                              }
+                                            ],
+                                            "totalExpense": 257755
+                                          }
+                                        }
+                                        """
+                        )
+                )
         )
 })
 public @interface ApiAssetResponse {
