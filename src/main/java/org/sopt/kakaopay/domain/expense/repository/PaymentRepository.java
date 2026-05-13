d105f057adbfc739fa8b9aa1a686dcad449c85ca
5eoyng <szoyoung@naver.com>
2026-05-13 12:09:18 +0900

    feat(#3): 카테고리별 지출 조회 쿼리 추가

--- src/main/java/org/sopt/kakaopay/domain/expense/repository/PaymentRepository.java
+++ src/main/java/org/sopt/kakaopay/domain/expense/repository/PaymentRepository.java
@@ -1,7 +1,28 @@
 package org.sopt.kakaopay.domain.expense.repository;
 
+import org.sopt.kakaopay.domain.expense.dto.ExpenseCategoryAmountDto;
 import org.sopt.kakaopay.domain.expense.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    @Query("""                                                                                                                                                                                                                       
+            SELECT new org.sopt.kakaopay.domain.expense.dto.ExpenseCategoryAmountDto(
+            p.paymentCategory, SUM(t.amount)
+            )
+            FROM Payment p JOIN p.transaction t
+            WHERE t.transactedAt >= :start
+            AND t.transactedAt < :end
+            AND t.includeInTotal = true
+            GROUP BY p.paymentCategory
+            """)
+
+    List<ExpenseCategoryAmountDto> findCategoryAmountsByPeriod(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end
+    );
 }
