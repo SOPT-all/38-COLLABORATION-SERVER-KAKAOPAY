c799ab73c26d81cbc26cd2fae85ee4cf4698cd01
Jyunee54 <jyunee54@gmail.com>
2026-05-13 18:27:49 +0900

    feat(#1): SQL 수정

--- src/main/java/org/sopt/kakaopay/domain/expense/repository/TransactionRepository.java
+++ src/main/java/org/sopt/kakaopay/domain/expense/repository/TransactionRepository.java
@@ -7,6 +7,12 @@ import org.springframework.data.repository.query.Param;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    @Query("SELECT COALESCE(SUM(t.amount), 0) FROM Transaction t WHERE t.includeInTotal = true AND YEAR(t.transactedAt) = :year AND MONTH(t.transactedAt) = :month")
+    @Query("""
+                SELECT COALESCE(SUM(t.amount), 0)
+                FROM Transaction t
+                WHERE t.includeInTotal = true
+                  AND YEAR(t.transactedAt) = :year
+                  AND MONTH(t.transactedAt) = :month
+            """)
     Long sumMonthlyExpense(@Param("year") int year, @Param("month") int month);
 }
