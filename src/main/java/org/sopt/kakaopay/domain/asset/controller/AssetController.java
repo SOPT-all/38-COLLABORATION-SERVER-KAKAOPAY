a17007a97eb27b531a08f2d9550be86c96aede2f
Jyunee54 <jyunee54@gmail.com>
2026-05-13 18:11:29 +0900

    feat(#1): AssetController 구현

--- src/main/java/org/sopt/kakaopay/domain/asset/controller/AssetController.java
+++ src/main/java/org/sopt/kakaopay/domain/asset/controller/AssetController.java
@@ -1,7 +1,12 @@
 package org.sopt.kakaopay.domain.asset.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.kakaopay.domain.asset.code.AssetSuccessCode;
+import org.sopt.kakaopay.domain.asset.dto.response.AssetResponse;
 import org.sopt.kakaopay.domain.asset.service.AssetService;
+import org.sopt.kakaopay.global.response.BaseResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AssetController {
 
     private final AssetService assetService;
+
+    @GetMapping
+    public ResponseEntity<BaseResponse<AssetResponse>> getAsset() {
+        return ResponseEntity.ok(BaseResponse.success(AssetSuccessCode.GET_ASSET, assetService.getAsset()));
+    }
 }
