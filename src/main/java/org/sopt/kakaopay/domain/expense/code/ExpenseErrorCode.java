554314138870afdd656aa414be1be9a6bca554ee
5eoyng <szoyoung@naver.com>
2026-05-14 13:04:03 +0900

    refactor(#6): 400 제거

--- src/main/java/org/sopt/kakaopay/domain/expense/code/ExpenseErrorCode.java
+++ src/main/java/org/sopt/kakaopay/domain/expense/code/ExpenseErrorCode.java
@@ -14,8 +14,8 @@ public enum ExpenseErrorCode implements ErrorCode {
     EXPENSE_ANALYSIS_NOT_FOUND(HttpStatus.NOT_FOUND, "EXP_ANALYSIS_404", "해당 연월의 소비 분석을 찾을 수 없습니다."),
     EXPENSE_ANALYSIS_INVALID_YEAR_MONTH(HttpStatus.BAD_REQUEST, "EXP_ANALYSIS_400", "yearMonth 형식이 올바르지 않습니다."),
 
-    EXPENSE_DETAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "EXP_DETAIL_404", "해당 지출 상세 내역을 찾을 수 없습니다."),
-    EXPENSE_DETAIL_BAD_REQUEST(HttpStatus.BAD_REQUEST, "EXP_DETAIL_400", "거래 ID 형식이 올바르지 않습니다.");
+    EXPENSE_DETAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "EXP_DETAIL_404", "해당 지출 상세 내역을 찾을 수 없습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String code;
